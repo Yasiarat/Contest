590114eaae0fff090b37c65d8678c29434ba344d
Yasiarat <98023179+Yasiarat@users.noreply.github.com>
2023-10-13 14:26:05 +0300

    Add files via upload

---
+++ test_11.c
@@ -0,0 +1,14 @@
+// Ратова Ярослава  группа 105 задание 11
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a[8][8];
+    int cnt = 1, tem = 0;
+    char c = 0;
+
+    while ((c = getchar()) != EOF) {
+        printf("%c", c);}
+    return 0;
+}
