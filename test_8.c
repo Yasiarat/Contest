1e435ba0da54e6df10c9eac279f09b624fedf120
Yasiarat <98023179+Yasiarat@users.noreply.github.com>
2023-12-07 23:03:06 +0300

    Update test_8.c
    добавила разность и настроила умножение

--- test_8.c
+++ test_8.c
@@ -2,18 +2,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define ABS(x) (((x) > 0) ? (x) : -(x))
 #define MIN(x, y) ((x > y) ? y : x)
 #define MAX(x, y) ((x > y) ? x : y)
 
-int sum(int a[], int b[], int mx) {
+// сумма
+void sum(int a[], int b[], int mx) {
     int res[10002];
     int i = 0;
+    int tem = 0;
+
     while (i < mx) {
-        res[mx - i] += (a[mx - i - 1] + b[mx - i - 1]) % 10;
+        res[mx - i] = (a[mx - i - 1] + b[mx - i - 1] + tem) % 10;
         i++;
-        res[mx - i] = (a[mx - i] + b[mx - i]) / 10;
+        tem = (a[mx - i] + b[mx - i] + tem) / 10;
     }
+    res[0] = tem;
     for (i = res[0] == 0 ? 1 : 0; i < mx + 1; i++) printf("%d", res[i]);
+}
+
+// разность
+void dif(int a[], int b[], int mx) {
+    int res[10002] = {0};
+    int i = 0, j = 0;
+    int tem = 0, flag  = 0;
+
+    while (i < mx) {
+        if (a[i] > b[i]) {
+            while (j < mx) {
+                if (a[mx - j - 1] >= b[mx - j - 1]) res[mx - j - 1] = a[mx - j - 1] - b[mx - j - 1];
+                else {
+                    res[mx - j - 1] = a[mx - j - 1] - b[mx - j - 1] + 10;
+                    a[mx - j - 2] -= 1;
+                }
+                j++;
+            }
+            flag = 1;
+            break;
+        }
+        else if(b[i] > a[i]) {
+            while (j < mx) {
+                if (b[mx - j - 1] >= a[mx - j - 1]) res[mx - j - 1] = b[mx - j - 1] - a[mx - j - 1];
+                else {
+                    res[mx - j - 1] = b[mx - j - 1] - a[mx - j - 1] + 10;
+                    b[mx - j - 2] -= 1;
+                }
+                j++;
+            }
+            for (j = 0; j < mx; j++) {
+                if (res[j] != 0){
+                    res[j] = -res[j];
+                    break;
+                }
+            }
+            flag = 1;
+            break;
+        }
+        i++;
+    }
+    if (flag == 0) {
+        printf("0");
+        return;
+    }
+    for (i = 0; i < mx; i++){
+            if (res[i] != 0) flag = 0;
+            if (flag == 0) printf("%d", res[i]);
+
+    }
+}
+
+// умножение
+void mult(int a[], int b[], int mx) {
+    
 }
 
 int main(void)
@@ -42,8 +102,6 @@ int main(void)
     }
     e_b = i - e_a - 1;
 
-    //sum(a, b, end_a, end_b);
-
     int mx = MAX(e_a, e_b);
 
     if (mx != e_b) {
@@ -60,7 +118,7 @@ int main(void)
         for (i = e_b - e_a - 1; i >= 0; i--) a[i] = 0;
     }
 
-    sum(a, b, mx);
+    dif(a, b, mx);
     /*
     for (i = 0; i < mx; i++) {
         printf("%d", a[i]);
